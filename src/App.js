304b51562aa4b057a52cc18bae9515e1e84a1b64
Rygel-XVI <micha232@umn.edu>
2018-11-26 18:23:59 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,11 @@ class App extends Component {
 
   //Your code here:
 
-
-
-
-
-
-
-
+componentDidMount() {
+  this.setState ({
+    handleAddTimer: this.handleAddTimer()
+  })
+}
 
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
