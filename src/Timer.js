d78161b6bba545c6dbf0489d552deb4cfe710141
github username <github email address>
2019-05-10 15:04:34 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 
 class Timer extends Component {
-
+  //ES7 syntax for setting state
   state = {
     time: 0,
     color: '#'+Math.floor(Math.random()*16777215).toString(16)
   }
 
   // add your code here
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000)
+  }
 
-
-
-
-
-
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
 
   render() {
 
