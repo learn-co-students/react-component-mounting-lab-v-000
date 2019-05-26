921e83204683e5cbb6018217e422473d870ed573
Pat  Pignataro <pat6284@msn.com>
2019-05-26 11:05:48 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import Timer from './Timer'
 
 class App extends Component {
@@ -10,53 +9,53 @@ class App extends Component {
   }
 
 
-  //Your code here:
+  state = {
 
+ 		timerIDs: []
 
+ 	};
+  //Your code here:	
 
+ 	state = {
+		timerIDs: []
+	};
 
+	componentDidMount() {
+		this.handleAddTimer();
+	}
 
+	// No need to modify anything in render or the class methods below
+	// Unless, of course, you're curious about how it all works
+	render() {
+		return (
+			<div className="App">
+				<h1>MultiTimer</h1>
+				<button onClick={this.handleAddTimer}>Add New Timer</button>
 
+				<div className="TimerGrid">{this.renderTimers()}</div>
+			</div>
+		);
+	}
 
+	// returns array of components written in JSX, mapped from this.state.timerIDs
+	renderTimers = () =>
+		this.state.timerIDs.map((id) => {
+			return <Timer key={id} id={id} removeTimer={this.removeTimer} />;
+		});
 
+	// adds a random number for timer ID
+	handleAddTimer = () => {
+		this.setState((prevState) => ({
+			timerIDs: [ ...prevState.timerIDs, Math.floor(Math.random() * 1000) ]
+		}));
+	};
 
-  // No need to modify anything in render or the class methods below
-  // Unless, of course, you're curious about how it all works
-  render() {
-
-    return (
-      <div className="App">
-        <h1>MultiTimer</h1>
-        <button onClick={this.handleAddTimer}>Add New Timer</button>
-
-        <div className="TimerGrid">
-          {this.renderTimers()}
-        </div>
-
-      </div>
-    );
-  }
-
-  // returns array of components written in JSX, mapped from this.state.timerIDs
-  renderTimers = () => this.state.timerIDs.map(id => {
-    return <Timer key={id} id={id} removeTimer={this.removeTimer} />
-  })
-
-  // adds a random number for timer ID
-  handleAddTimer = () => {
-    this.setState(prevState => ({
-      timerIDs: [...prevState.timerIDs, Math.floor(Math.random()*1000)]
-    }))
-  }
-
-  // removeTimer updates state, removing any timer that matches the provided author
-  removeTimer = id => {
-    this.setState(prevState => ({
-      timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
-    }))
-  }
-
-
+	// removeTimer updates state, removing any timer that matches the provided author
+	removeTimer = (id) => {
+		this.setState((prevState) => ({
+			timerIDs: prevState.timerIDs.filter((timer_id) => timer_id !== id)
+		}));
+	};
 }
 
 export default App;
