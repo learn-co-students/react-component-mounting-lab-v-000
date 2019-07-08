a54df2bac63c90d6763945e22b9b3335f6a53adc
Jingru Zhang <zhang.jingru.93@gmail.com>
2019-07-08 17:02:22 +0800

    Done.

--- src/App.js
+++ src/App.js
@@ -11,14 +11,15 @@ class App extends Component {
 
 
   //Your code here:
+  componentDidMount() {
+    this.handleAddTimer();
+  }
+  
 
-
-
-
-
-
-
-
+  componentWillUnmount() {
+    this.removeTimer();
+  }
+  
 
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
