5b762e347fdc106bbed3e777b17ad8df43756bed
Christopher DiStasio <cadistasio@gmail.com>
2019-08-18 15:16:03 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-    // this.interval = setInterval(this.fetchWeather, 15000);
+    this.handleAddTimer()
+    // setInterval(this.handleAddTimer, 1000);
+    // nope, this actually adds a new timer every second :)
+    //In addition to being for learning in this lab, we're using componentDidMount before the render so that it is a separate process from the render, for optimization purposes?
   }
 
 
@@ -22,6 +25,7 @@ class App extends Component {
 
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
+
   render() {
 
     return (
