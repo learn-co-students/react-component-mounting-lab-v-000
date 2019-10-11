44ba14f8d0df3716039c3efaa6edfc1d59658169
valeria campilongo <cvaleriac@hotmail.it>
2019-10-11 13:47:06 +0000

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -8,45 +8,49 @@ class Timer extends Component {
   }
 
   // add your code here
+  componentDidMount(){
+     this.interval = setInterval(this.clockTick, 1000)
+   }
+
+   componentWillUnmount(){
+     clearInterval(this.interval)
+   }
 
 
 
 
+   render() {
+
+     const { time, color, className } = this.state
+     return (
+       <section className="Timer" style={{background: color}}>
+
+         <h1>{ time }</h1>
+         <button onClick={ this.stopClock }>Stop</button>
+         <aside className="mountText">Mounted</aside>
+         <small onClick={ this.handleClose }>X</small>
+
+       </section>
+     );
+   }
+
+   //clock functions
+   clockTick = () => {
+     this.setState(prevState => ({
+       time: prevState.time+1
+     }))
+   }
+
+   stopClock = () => {
+     clearInterval(this.interval)
+   }
+
+   // for the 'x' button,
+   handleClose = () => {
+     this.props.removeTimer(this.props.id)
+   }
 
 
+ }
 
-  render() {
-
-    const { time, color, className } = this.state
-    return (
-      <section className="Timer" style={{background: color}}>
-
-        <h1>{ time }</h1>
-        <button onClick={ this.stopClock }>Stop</button>
-        <aside className="mountText">Mounted</aside>
-        <small onClick={ this.handleClose }>X</small>
-
-      </section>
-    );
-  }
-
-  //clock functions
-  clockTick = () => {
-    this.setState(prevState => ({
-      time: prevState.time+1
-    }))
-  }
-
-  stopClock = () => {
-    clearInterval(this.interval)
-  }
-
-  // for the 'x' button,
-  handleClose = () => {
-    this.props.removeTimer(this.props.id)
-  }
-
-
-}
-
-export default Timer;
+ export default Timer;
