909fc4d0636ffeedc2a308b7a5f8d3fab957946d
Sara Harvey <saraharveynyc@gmail.com>
2020-11-30 13:52:40 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,48 @@ import Timer from './Timer'
 
 class App extends Component {
 
-  //no props being used here, so we can use the shorthand declaration of state
   state = {
     timerIDs: []
   }
 
+  componentDidMount() {
+    this.handleAddTimer()
+  }
 
-  //Your code here:
+  render() {
+
+    return (
+      <div className="App">
+        <h1>MultiTimer</h1>
+        <button onClick={this.handleAddTimer}>Add New Timer</button>
+
+        <div className="TimerGrid">
+          {this.renderTimers()}
+        </div>
+
+      </div>
+    );
+  }
+
+  renderTimers = () => this.state.timerIDs.map(id => {
+    return <Timer key={id} id={id} removeTimer={this.removeTimer} />
+  })
+
+  handleAddTimer = () => {
+    this.setState(prevState => ({
+      timerIDs: [...prevState.timerIDs, Math.floor(Math.random()*1000)]
+    }))
+  }
+
+  removeTimer = id => {
+    this.setState(prevState => ({
+      timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
+    }))
+  }
+
+}
+
+export default App;
 
 
 
