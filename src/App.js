47b2c08977757e066d41679997ae256f975a014d
Eric Johnson <ericjohnson0216@icloud.com>
2021-01-22 18:47:36 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 
 import Timer from './Timer'
 
+
+
+
+
+
+
 class App extends Component {
 
   //no props being used here, so we can use the shorthand declaration of state
@@ -12,14 +18,9 @@ class App extends Component {
 
 
   //Your code here:
-
-
-
-
-
-
-
-
+componentDidMount() {
+  this.handleAddTimer()
+}
 
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
